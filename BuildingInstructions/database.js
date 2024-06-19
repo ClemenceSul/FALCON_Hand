947e603a54dc186908056f763f3a5eaaed1a46a3
Clemence <clemence.sulmont20@imperial.ac.uk>
2024-06-20 00:59:35 +0100

    moving the pages to dedicated folder

--- BuildingInstructions/database.js
+++
@@ -1,9 +0,0 @@
-const database = {
-    "tab1": [
-        "./Guide_Images/PrepPieces/Fingertip/Slide2.PNG",
-        "./Guide_Images/PrepPieces/Fingertip/Slide3.PNG",
-        "./Guide_Images/PrepPieces/Fingertip/Slide4.PNG",
-        "./Guide_Images/PrepPieces/Fingertip/Slide5.PNG"
-    ]
-};
-
