47a4cdae6d8cb0957fcbcd0f0b2a010885409b75
Clemence <clemence.sulmont20@imperial.ac.uk>
2024-06-20 08:34:06 +0100

    Adding license header

--- BuildingInstructions/Other_Resources/tabs_content.js
+++ BuildingInstructions/Other_Resources/tabs_content.js
@@ -1,3 +1,11 @@
+/*
+This file is part of FALCON_Hand.
+
+FALCON_Hand is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License.
+To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/. 
+*/
+
+
 const database = {
     "prep": {
         "tab1": {   //fingertip
